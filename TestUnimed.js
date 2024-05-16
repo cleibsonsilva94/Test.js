ca07a1c349471ddb611860799c11a766871266f8
Cleibson Silva <156372072+cleibsonsilva94@users.noreply.github.com>
2024-05-16 18:33:40 -0300

    Update TestUnimed.js

--- TestUnimed.js
+++ TestUnimed.js
@@ -1,21 +1,20 @@
-const { Builder, By, until } = require('selenium-webdriver'); // Importação dos recursos necessários do selenium-webdriver
+const { Builder, By, until } = require('selenium-webdriver');
 
 // PREPARAÇÃO DO ANBIENTE PARA TESTE DE FORMULARIO. 
 async function example() {
-  let driver = await new Builder().forBrowser('chrome').build(); // Inicia uma instância do navegador Chrome
+  let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('https://www.unimed.coop.br/site/'); // Abre o site com a URL indicada
+    await driver.get('https://www.unimed.coop.br/site/'); 
     await driver.sleep(20000);
-    await driver.findElement(By.xpath("//div[@class='cf1lHZ cf2MAH cf3l36']//*[@aria-label='Aceitar todos']")).click(); // Fecha o pop-up de cookies abaixo da tela
+    await driver.findElement(By.xpath("//div[@class='cf1lHZ cf2MAH cf3l36']//*[@aria-label='Aceitar todos']")).click();
 
     //VAI ATÉ PÉ DA PÁG 
 
     // Encontra o elemento e rola até ele. 
-    let elemento = await driver.findElement(By.xpath("//p[contains(@class, 'menuAcessoRapid') and contains(text(), 'Serviços')]")); // Segue até elemento
-    await driver.executeScript("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", elemento); // block: 'center' centraliza o elemento na janela do navegador.
-    // Aguarde um breve momento para garantir que o elemento esteja visível após a rolagem
+    let elemento = await driver.findElement(By.xpath("//p[contains(@class, 'menuAcessoRapid') and contains(text(), 'Serviços')]"));
+    await driver.executeScript("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", elemento); 
     await driver.sleep(5000); 
-    await driver.findElement(By.xpath("//a[contains(@class, 'menuAcessoRapido') and contains(text(), 'Planos')]")).click(); //Clica no "Plano" usando um xpath usando o texto.
+    await driver.findElement(By.xpath("//a[contains(@class, 'menuAcessoRapido') and contains(text(), 'Planos')]")).click();
 
     //JÁ NA PÁG DO FORMULARIO
 
@@ -27,86 +26,17 @@ async function example() {
     await driver.findElement(By.xpath("(//div[@class='form-group'])[2]")).click();
     await driver.sleep(2000);
     await driver.findElement(By.xpath("(//input[@name='nome'])[1]")).sendKeys('Cleibson Silva');
+    await driver.findElement(By.xpath("(//div[@class='form-group'])[3]")).click();
+    await driver.sleep(2000);
+    await driver.findElement(By.xpath("(//input[@name='email'])")).sendKeys('Test01@gmail.com');
+    await driver.findElement(By.xpath("(//div[@class='form-group'])[4]")).click();
+    await driver.sleep(2000);
+    await driver.findElement(By.xpath("(//input[@name='telefone'])")).sendKeys('8199318-7771');
+    await driver.findElement(By.xpath("(//span[@class='Select-arrow-zone'])[1]")).click();
+    await driver.wait(until.elementLocated(By.xpath("//div[contains(text(),'Pernambuco')]")), 10000);
+    await driver.findElement(By.xpath("//div[contains(text(),'Pernambuco')]")).click();
+    await driver.sleep(2000);
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-        // Escolhendo um produto depois do search 
-        await driver.findElement(By.xpath("//img[contains(@alt, 'Máquina Elétrica de Cortar Cabelo Cadence CAB181 220V')]")).click();
-        await driver.sleep(5000);
-        await driver.findElement(By.xpath("//div[@class='styles__BoxButtons-sc-f087e8f-2 bZyBkQ']//button")).click();
-        await driver.sleep(5000);
-        await driver.findElement(By.xpath("//button[@class='styles__ButtonSuccess-sc-ae78ac84-4 bXzTub']")).click();
-        await driver.sleep(5000);
-    
-        // Guarda o nome do produto
-        let produtoNoCarrinhoElement = await driver.findElement(By.xpath("//h3[@class='styles__TitleProduct-sc-432ccc7e-5 dXlifT']"));
-        let produtoNoCarrinhoTexto = await produtoNoCarrinhoElement.getText();
-    
-        // Guarda o valor do produto
-        let valorProdutoElement = await driver.findElement(By.xpath("//h3[@class='styles__TextPrice-sc-432ccc7e-11 gDOEip']"));
-        let valorProdutoNaPagTexto = await valorProdutoElement.getText();
-        let valorProdutoNaPag = parseFloat(valorProdutoNaPagTexto.replace(/[^\d.,]/g, '').replace(',', '.'));
-    
-        // Retornando à página inicial
-        await driver.findElement(By.xpath('//div[@id="logo-wapper"]')).click();
-        await driver.sleep(5000);
-        await driver.findElement(By.xpath("//*[contains(@class, 'styles__BaseHyperLink-sc-7fed0')]")).click();
-        await driver.sleep(5000);
-    
-        // Verificando o carrinho
-        await driver.findElement(By.xpath("//div[@data-cy='button-cart-modal-cart']")).click();
-        await driver.sleep(5000);
-    
-        // Nome e Valor do produto
-        let nomeProduto = "Máquina Elétrica de Cortar Cabelo Cadence CAB181 220V";
-        let valorProduto = 89.90;
-    
-        //Verificação Final por meio do condicional
-        if (produtoNoCarrinhoTexto.includes(nomeProduto) && valorProdutoNaPag === valorProduto) {
-          console.log(`O produto, ${nomeProduto}, está no carrinho e o valor está correto: R$${valorProduto.toFixed(2)} centavos.`);
-        } else if (produtoNoCarrinhoTexto.includes(nomeProduto)) {
-          console.log(`O produto, ${nomeProduto}, está no carrinho mas o valor não está correto! O valor presente é R$${valorProdutoNaPag.toFixed(2)} centavos e não R$${valorProduto.toFixed(2)} centavos.`);
-        } else {
-          console.log(`O produto, ${nomeProduto}, não está no carrinho. O produto presente é: ${produtoNoCarrinhoTexto}. Verifique por favor.`);
-        }
-        */
-    // Fechando o navegador
   } finally {
     await driver.quit();
   }
