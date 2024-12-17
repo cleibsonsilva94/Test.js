57334b2e282c0c55d5ddf73a1e9967e816441bd1
cleibsonsilva94 <cleibsonslima@gmail.com>
2024-12-17 11:50:50 -0300

    Fixed xpaths

--- TestCucumberCart - Fun/features/support/xpaths.js
+++ TestCucumberCart - Fun/features/support/xpaths.js
@@ -6,11 +6,11 @@ module.exports = {
     XPATH_PRODUCT_IMAGE: "//img[contains(@alt, 'Máquina Elétrica de Cortar Cabelo Cadence CAB181 220V')]",
     XPATH_ADD_TO_CART_BUTTON: "//div[@data-cy='box-product-box-buttons']",
     XPATH_PROCEED_TO_CART_BUTTON: "//button[@class='styles__ButtonSuccess-sc-78d4639c-4 cJdVyu']",
-    XPATH_PRODUCT_NAME_IN_CART: "//h1[@class='styles__Title1-sc-c3e89460-19 iQtjHM']",
-    XPATH_PRODUCT_PRICE_IN_CART: "//h2[@class='Product__PriceValue-sc-d1a8057a-7 jJBrQI']",
-    XPATH_LOGO: '//div[@class="styles__FcLogoBlackWrapper-sc-4b37a4d6-23 kJnCcO"]',
+    XPATH_PRODUCT_NAME_IN_CART: "//h3[@class='styles__TitleProduct-sc-8ac0a507-5 gkyEfO']",
+    XPATH_PRODUCT_PRICE_IN_CART: "//h3[@class='styles__TextPrice-sc-8ac0a507-11 dZjvJE']",
+    XPATH_LOGO: '//div[@id="logo-wapper"]',
     XPATH_MENU_ITEM: "//*[contains(@class, 'styles__BaseHyperLink-sc-7fed0')]",
-    XPATH_CART_BUTTON: "(//div[@class='Container-sc-b91fe638-0 XjjIe']//*[@aria-label='Carrinho'])[1]",
+    XPATH_CART_BUTTON: "//span[@data-cy='cart-hyper-link']",
     XPATH_CART: "//div[contains(@class, 'ButtonCart-sc')]"
   };
   
